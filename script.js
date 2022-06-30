df0b8584b32a0e2669806f0dadf060a873af2bd8
Nick <Nick_II2000@mail.ru>
2022-06-30 09:12:45 +0600

    4-th commit

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ const names = ['Ivan', 'Ann', 'Ksenia', 'Voldevart', 'Nick', 'Svetlana'];
 const shortNames = names.filter(function (name) {
     return name.length < 5;
 });
-
 console.log(shortNames);
 
 const shortNames2 = names.filter(name => name.length >= 5);
-
 console.log(shortNames2);
+
+// map
+
+let answers = ['IvAn', 'AnnA', 'HeLLO'];
+
+const result = answers.map(item => item.toLowerCase());
+console.log(result);
+
+answers = answers.map(item => item.toLowerCase());
+console.log(answers);
