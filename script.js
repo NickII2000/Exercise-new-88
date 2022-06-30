87279773d7021fcc2158716b7ef3c480bc72e260
Nick <Nick_II2000@mail.ru>
2022-06-30 09:00:35 +0600

    2-th commit

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+// filter
+
+const names = ['Ivan', 'Ann', 'Ksenia', 'Voldevart', 'Nick', 'Svetlana'];
+
+const shortNames = names.filter(function (name) {
+    return name.length < 5;
+});
+
+console.log(shortNames);
